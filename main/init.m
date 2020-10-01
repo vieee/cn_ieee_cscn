0dfb56456db905461d736cf386f1d7603e783206
TanShets <60138962+TanShets@users.noreply.github.com>
2020-10-01 18:37:04 +0530

    Add files via upload

---
+++ main/init.m
@@ -0,0 +1,23 @@
+addpath("./cn_ieee_cscn/main/GridCell");
+addpath("./cn_ieee_cscn/main/StructArray");
+# This simply creates the major area and randomly creates the sensors in separate positions
+function [rect, sensors] = init()
+	width = input("Please state the length of the area: ");
+	height = input("Please state the width of the area: ");
+	rect = GridCell(0, 0, width, height);
+	sensors = StructArray();
+	n = input("Please enter the number of Sensors: ");
+	r = input("Please enter the range of each sensor: ");
+	for i = 1 : 1 : n
+		sensor = Sensor(width * rand(1), height * rand(1), r);
+		sensors = add_struct(sensors, sensor);
+		# disp(sensors(i + 1));
+	endfor
+endfunction
+
+#{
+[rect, sensors] = init();
+for i = 1 : 1 : length(sensors)
+	disp(sensors(i));
+endfor
+#}
