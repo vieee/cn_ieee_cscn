a4d215f3efd148e51c324715643f85a5d0570615
TanShets <60138962+TanShets@users.noreply.github.com>
2020-10-01 18:41:51 +0530

    Update init.m

--- main/init.m
+++ main/init.m
@@ -1,6 +1,6 @@
-addpath("./cn_ieee_cscn/main/GridCell");
-addpath("./cn_ieee_cscn/main/StructArray");
-# This simply creates the major area and randomly creates the sensors in separate positions
+#addpath("./cn_ieee_cscn/main/GridCell");
+#addpath("./cn_ieee_cscn/main/StructArray");
+#This simply creates the major area and randomly creates the sensors in separate positions
 function [rect, sensors] = init()
 	width = input("Please state the length of the area: ");
 	height = input("Please state the width of the area: ");
@@ -15,10 +15,8 @@ function [rect, sensors] = init()
 	endfor
 endfunction
 
-#{
-This is how we implement it
-[rect, sensors] = init();
-for i = 1 : 1 : length(sensors)
-	disp(sensors(i));
-endfor
-#}
+#This is how we implement it
+#[rect, sensors] = init();
+#for i = 1 : 1 : length(sensors)
+#	disp(sensors(i));
+#endfor
