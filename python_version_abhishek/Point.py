b9c50c3b2850b52fbf7c211aa218afbc8031635c
ilvanip <24abhish@gmail.com>
2020-10-07 01:27:14 +0530

    Added GridCell.Improved point functionality

--- python_version_abhishek/Point.py
+++ python_version_abhishek/Point.py
@@ -25,7 +25,20 @@ class Point():
 	#Deep copy a point and return it.
 	#Use this , or use the 'deepcopy' function from the 'copy' module.
 	def copy(self):
-		return POint(self._x,self._y)
+		return Point(self._x,self._y)
+
+	#Return the square of the distance of a point from the object.
+	def distance_squared(self,point):
+		type_check(point,(Point,),'Argument must be a point')
+		return (self._x-point._x)**2+(self._y-point._y)**2
+
+	#Definition of equality between two points.
+	#P1==P2 is defined as:
+	def __eq__(self,point):
+		type_check(point,(Point,),'Argument must be a point')
+		return (self._x==point._x and self._y==point._y)
+
+
 
 
 #Tests.
@@ -35,4 +48,14 @@ if __name__=='__main__':
 #	p=Point(x,y)
 	p=Point(1,1.901)
 	print(p)
+
 	print(p.coordinates())
+
+	p1=Point(10,10)
+	p2=Point(20,20)
+	print(p1.distance_squared(p2))
+
+	p1=Point(1,2)
+	p2=Point(1,2)
+	print(p1 is p2)
+	print(p1==p2)
--- python_version_abhishek/Sensor.py
+++ python_version_abhishek/Sensor.py
@@ -1,5 +1,5 @@
 #Global variables to read
-import Globals
+from Globals import sensor_sensing_radius as SENSOR_SENSING_RADIUS
 #Import type and condition checking barriers.
 from check import *
 #Import the point class
@@ -8,13 +8,25 @@ from Point import Point
 #I'm not calling range of the sensor by its name because python has a range function
 class Sensor:
 	#Radius is a static value as the radii of all sensors will be the same
-	radius=Globals.sensor_sensing_radius
+	radius=SENSOR_SENSING_RADIUS
 	def __init__(self,center_coordinates,lifetime):
 		#Perform error checking first.
-		type_check(coordinates,(Point,),'Sensor coordinates must be an object of type \'Point\'.')
+		type_check(center_coordinates,(Point,),'Sensor coordinates must be an object of type \'Point\'.')
 		type_check(lifetime,(int,float),'Lifetime must be a positive integer or float')
 		value_check(lifetime>0,'Lifetime must greater than 0')
-		self.coordinates=coordinates
+		self.center=center_coordinates.copy()
 		#The lifetime will be different for each sensor.
 		self.remaining_lifetime=lifetime
 
+	#Can this sensor sense a point 'point'?
+	def can_sense(self,point):
+		return self.center.distance_squared(point)<=Sensor.radius**2
+
+	#Object representation in human readable form.
+	def __repr__(self):
+		return f'Sensor:[Center:[{self.center}],Radius:{Sensor.radius},Remaining Time:{self.remaining_lifetime}]'
+
+
+p=Point(1,2)
+s=Sensor(p,10)
+print(s)
