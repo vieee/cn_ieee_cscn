dfd2ad7564898713a2840c1427ac523ebc921cdd
Deepak <dgyadv1782000@gmail.com>
2020-10-10 16:03:55 +0530

    add logic for final display

---
+++ merge_n_display/show_final_message.py
@@ -0,0 +1,27 @@
+def show_up(coverset_list):
+  # coverset_list: contains list data denoting coversets in each grid cell
+  result_coverset = []
+  length = len(coverset_list)
+  time_of_work = 0
+  index = 0
+  k = 0
+  small = 100000
+  while true:
+    for i in range(length):
+      if coverset_list[i][0][1] < small:
+        small = coverset_list[i][0][1]
+        index = i
+    
+    for i in range(length):
+      coverset_list[i][0][1] -= small
+      if coverset_list[i][0][1] <= 0:
+        result_coverset[k].append(coverset_list[i][0][0])
+        coverset_list[i].pop(0)
+
+    k += 1
+    time_of_work += small
+    for i in range(length):
+      if len(coverset_list[i]) == 0:
+        # Coversets done,..
+        alert_popup("Final Observation:-", "Time " + str(time_of_work), listToString(result_coverset))
+
