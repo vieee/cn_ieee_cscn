b135fcb605ef27dedc14db2776c250cc9b8450e8
ilvanip <24abhish@gmail.com>
2020-10-25 12:09:52 +0530

    Added summation across all coversets.

--- final_integrated/run.py
+++ final_integrated/run.py
@@ -17,31 +17,21 @@ if __name__=='__main__':
 	#All global variables have been set.
 
 	#Set all sensors here.
-	#Jashs/Tanishs algo.
 	init_sensors.set_sensor_coordinates()
 
 	#Set the value of K.
 	K=int(input('Enter K:'))
 
-	
 	grid_cells=create_grids.create_grids(K)
 
 	coversets_of_cells=find_coversets.find_coversets(grid_cells)
 
-	'''
-	print(len(coversets_of_cells))
-	print('................')
-	print(coversets_of_cells[0][-1],coversets_of_cells[0][0])
-	print('................')
-	print(coversets_of_cells[1][-1],coversets_of_cells[1][0])
-	print('................')
-	print(coversets_of_cells[2][-1],coversets_of_cells[2][0])
-	print('................')
-	print(coversets_of_cells[3][-1],coversets_of_cells[3][0])
-	print('................')
-#	'''
-
 	final_result=merge.merge_coversets(coversets_of_cells)
 
-	print(final_result)
+	max_time=0
+	for k,cs in enumerate(final_result):
+		print(f'Time of coverset {k}',cs.lifetime)
+		max_time+=cs.lifetime
+	print('Maximum lifetime of the network',max_time)
+
 
